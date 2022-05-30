98783b69db5d3a41b92f8f92816b09801d5e6c35
Ben Finlay <benjamin.finlay@gmail.com>
2022-05-30 11:11:33 -0400

    Adds required code to exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Homer", last_name: "Simpson", hourly_rate: 80)
+@store1.employees.create(first_name: "Dave", last_name: "Dude", hourly_rate: 100)
+@store2.employees.create(first_name: "Bea", last_name: "Beau", hourly_rate: 120)
+@store2.employees.create(first_name: "April", last_name: "June", hourly_rate: 140)
+@store2.employees.create(first_name: "Cindy", last_name: "Lu", hourly_rate: 160)
